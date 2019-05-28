d3b21c0b50d7a95f7112cba13f28605a8ffc2552
JB <21400667@handong.edu>
2019-05-28 23:48:13 +0900

    HW6

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,18 +1,37 @@
 package edu.handong.analysis;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.commons.csv.CSVRecord;
+
 import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
 import edu.handong.analysise.utils.NotEnoughArgumentException;
 import edu.handong.analysise.utils.Utils;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 public class HGUCoursePatternAnalyzer {
 
 	private HashMap<String, Student> students;
+	String input;
+	String output;
+	String analysis;
+	String startyear;
+	String endyear;
+	String coursecode;
+	boolean help;
+
+	int numOfFiles = 0;
 	/**
 	 * This method runs our analysis logic to save the number courses taken by each student per semester in a result file.
 	 * Run method must not be changed!!
@@ -20,50 +39,119 @@ public class HGUCoursePatternAnalyzer {
 	 */
 	public void run(String[] args) {
 		
-		try {
-			// when there are not enough arguments from CLI, it throws the NotEnoughArgmentException which must be defined by you.
-			if(args.length<2)
-				throw new NotEnoughArgumentException();
-		} catch (NotEnoughArgumentException e) {
-			System.out.println(e.getMessage());
-			System.exit(0);
-		}
-		
-		String dataPath = args[0]; // csv file to be analyzed
-		String resultPath = args[1]; // the file path where the results are saved.
-		
-		ArrayList<String> lines = Utils.getLines(dataPath, true);
-
-		students = loadStudentCourseRecords(lines);
-		
-		//To sort HashMap entries by key values so that we can save the results by student ids in ascending order.
-		Map<String, Student> sortedStudents = new TreeMap<String,Student>(students); 
-
-		// Generate result lines to be saved.
-		ArrayList<String> linesToBeSaved = countNumberOfCoursesTakenInEachSemester(sortedStudents);
-		
-		// Write a file (named like the value of resultPath) with linesTobeSaved.
-		Utils.writeAFile(linesToBeSaved, resultPath);
-	}
+		Options options = createOptions();
 	
+		if(parseOptions(options, args)){
+			if (help){
+				printHelp(options);
+				return;
+			}
+			//input
+			String dataPath = input;
+			String resultPath = output;
+			
+			ArrayList<CSVRecord> lines = Utils.getLines(dataPath, true);
+			students = loadStudentCourseRecords(lines, startyear, endyear);
+			Map<String, Student> sortedStudents = new TreeMap<String,Student>(students); 
+			//1번을 선택한경우
+			if(analysis.equals("1")) {
+				// Generate result lines to be saved.
+				ArrayList<String> linesToBeSaved = countNumberOfCoursesTakenInEachSemester(sortedStudents);
+				// Write a file (named like the value of resultPath) with linesTobeSaved.
+				Utils.writeAFile(linesToBeSaved, resultPath);				
+			}
+			//2번을 선택한 경우
+			else {
+				//Year,Semester,CourseCode, CourseName,TotalStudents,StudentsTaken,Rate
+				HashMap<String, Integer> result = new HashMap<String,Integer>();
+				HashMap<String, Integer> totalresult = new HashMap<String,Integer>();
+				ArrayList<String> linesToBeSaved = new ArrayList<String>();
+				String courseName = null;
+				
+				for(CSVRecord temp : lines) {
+					if(temp.get(4).equals(coursecode)) {
+						courseName = temp.get(5);
+						break;
+					}
+				}
+				
+				//모든 스튜던트들을 검사한다.
+				for( String key : sortedStudents.keySet()) {
+					Student tempStudent = sortedStudents.get(key);
+					//스튜던트가 가지고 있는 모든 코스를 검사한다.
+					for(Course tempCourse : tempStudent.getCoursesTaken()) {
+						//임시 키값 세팅
+						String tempString = tempCourse.getYearTaken() + "-" + tempCourse.getSemesterCourseTaken();
+						//만약 코스코드가 원하는 코스코드와 같다면
+						if(tempCourse.getCourseCode().equals(coursecode)) {
+							if(result.isEmpty()) {
+								result.put(tempString, 1);
+							}
+						else {
+							if(result.containsKey(tempString)) {
+								result.put(tempString, result.get(tempString)+1);
+							}
+							else {
+								result.put(tempString, 1);
+							}
+						}
+					}
+				}
+			}
+				for( String resultKey : result.keySet()) {
+					totalresult.put(resultKey, 0);
+					for( String studentKey : sortedStudents.keySet()) {
+						if(sortedStudents.get(studentKey).getSemestersByYearAndSemester().containsKey(resultKey)) {
+							totalresult.put(resultKey, totalresult.get(resultKey)+1);
+						}
+					}
+				}
+				
+				linesToBeSaved.add("Year,Semester,CourseCode, CourseName,TotalStudents,StudentsTaken,Rate");
+				Map<String, Integer> finalResult = new TreeMap<String,Integer>(result);
+				
+				for(String resultkey : finalResult.keySet()) {
+					
+					float Rate = Math.round(((float)(result.get(resultkey) / (float)totalresult.get(resultkey))*100));
+					System.out.println(Rate);
+					
+					String tempString = resultkey.split("-")[0] + "," 
+										+resultkey.split("-")[1] + ","
+										+coursecode + ","
+										+courseName + ","
+										+totalresult.get(resultkey) + ","
+										+result.get(resultkey) + ","
+										+String.valueOf(Rate) + "%";
+					linesToBeSaved.add(tempString); 
+				}
+				Utils.writeAFile(linesToBeSaved, resultPath);
+		}	
+	}
+}
+		
 	/**
 	 * This method create HashMap<String,Student> from the data csv file. Key is a student id and the corresponding object is an instance of Student.
 	 * The Student instance have all the Course instances taken by the student.
 	 * @param lines
 	 * @return
 	 */
-	private HashMap<String,Student> loadStudentCourseRecords(ArrayList<String> lines){
+	private HashMap<String,Student> loadStudentCourseRecords(ArrayList<CSVRecord> lines, String startyear, String endyear){
 		
 		// TODO: Implement this method
 		HashMap<String,Student> temp = new HashMap<String,Student>();
 	
-		for (String line : lines) {
+		for (CSVRecord line : lines) {
 			//라인정보를 course로 바꿔줌
 			Course tempCourse = new Course(line);
 			Student tempStudent = new Student(tempCourse.getStudentId());
 			tempStudent.addCourse(tempCourse);
+			
+			int start = Integer.parseInt(startyear);
+			int end = Integer.parseInt(endyear);
+			int tempyear = tempCourse.getYearTaken();
+			
 			//만약 처음으로 들어오는 값이면
-			if(temp.isEmpty()) {
+			if(temp.isEmpty()&&tempyear>=start&&tempyear<=end) {
 				//스튜던트에 course와 id를 셋해준뒤 그냥 넣어준다.
 				temp.put(tempStudent.getStudentId(), tempStudent);
 			} else {
@@ -72,7 +160,8 @@ public class HGUCoursePatternAnalyzer {
 					//그 스튜던트에 코스를 추가해줍니다.
 					temp.get(tempStudent.getStudentId()).addCourse(tempCourse);
 				}else {
-					temp.put(tempStudent.getStudentId(), tempStudent);
+					if(tempyear>=start&&tempyear<=end)
+						temp.put(tempStudent.getStudentId(), tempStudent);
 				}
 			}
         }
@@ -127,4 +216,91 @@ public class HGUCoursePatternAnalyzer {
 				
 		return result; // do not forget to return a proper variable.
 	}
+
+	
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			
+			input = cmd.getOptionValue("i");
+			output = cmd.getOptionValue("o");
+			analysis = cmd.getOptionValue("a");
+			startyear = cmd.getOptionValue("s");
+			endyear = cmd.getOptionValue("e");
+			coursecode = cmd.getOptionValue("c");
+			help = cmd.hasOption("h");
+
+		} catch (Exception e) {
+			printHelp(options);
+			return false;
+		}
+
+		return true;
+	}
+
+	// Definition Stage
+	private Options createOptions() {
+		Options options = new Options();
+		
+		options.addOption(Option.builder("i").longOpt("input")
+				.desc("Set an input file path")
+				.hasArg()
+				.argName("Input path")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("o").longOpt("output")
+				.desc("Set an output file path")
+				.hasArg()
+				.argName("Output path")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("a").longOpt("analysis")
+				.desc("1: Count courses per semester, 2: Count per course name and year")
+				.hasArg()
+				.argName("Analysis option")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("c").longOpt("coursecode")
+				.desc("Course code for '-a 2' option")
+				.hasArg()
+				.argName("course code")
+				//.required()
+				.build());
+		
+		options.addOption(Option.builder("s").longOpt("startyear")
+				.desc("Set the start year for analysis e.g., -s 2002")
+				.hasArg()
+				.argName("Start year for analysis")
+				.required()
+				.build());
+		
+		options.addOption(Option.builder("e").longOpt("endyear")
+				.desc("Set the end year for analysis e.g., -e 2005")
+				.hasArg()
+				.argName("End year for analysis")
+				.required()
+				.build());
+		
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("h").longOpt("help")
+		        .desc("Help")
+		        .build());
+
+		return options;
+	}
+	
+	private void printHelp(Options options) {
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "HGU Course Analyzer";
+		String footer ="";
+		formatter.printHelp("HGU Course Analyzer", header, options, footer, true);
+	}
+
 }
+
+
--- src/main/java/edu/handong/analysise/utils/Utils.java
+++ src/main/java/edu/handong/analysise/utils/Utils.java
@@ -1,35 +1,59 @@
 package edu.handong.analysise.utils;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+
 import java.io.PrintWriter;
+import java.io.Reader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class Utils {
 	
-	public static ArrayList<String> getLines(String file, boolean removeHeader){	
-		ArrayList<String> temp = new ArrayList<String>();
-		Scanner inputStream = null;
+	public static ArrayList<CSVRecord> getLines(String file, boolean removeHeader) {	
+		
+		//리턴값
+		ArrayList<CSVRecord> temp = new ArrayList<CSVRecord>();
+		FileReader fileReader = null;
+		CSVParser csvFileParser = null;
+		CSVFormat csvFileFormat = CSVFormat.EXCEL
+				.withIgnoreEmptyLines(true)
+				.withTrim();
 		try {
-			//file이 존재하나 체크
-			File fileCheck = new File(file);
-			if(fileCheck.exists() == false) {
-				throw new NotEnoughArgumentException("The file path does not exist. Please check your CLI argument!");
+			 fileReader = new FileReader(file);	
+			 csvFileParser = csvFileFormat.parse(fileReader);
+			 }catch(Exception e) {
+			e.printStackTrace();
+		}
+		//템프에 넣어주기 위해서 이터레이터로 만들어줌
+		Iterator<CSVRecord> iter = csvFileParser.iterator();
+		//템프에 값 넣어줌
+		try {
+			while(iter.hasNext()) {
+				temp.add(iter.next());
 			}
-			inputStream = new Scanner(new File(file));
-			
-			while (inputStream.hasNextLine ()) {
-				String line = inputStream.nextLine();
-				temp.add(line);		
-			}
-			if(removeHeader == true) temp.remove(0);
-		}  catch (FileNotFoundException e) {
+		}catch(Exception e){
+			System.out.println("iter.hasNext() exception");
+		}
+/*		catch (FileNotFoundException e) {
 			System.out.println ("Error opening the file " + file);
 			System.exit (0);
 		}catch(NotEnoughArgumentException e) {
 			System.out.println(e);
+		}*/
+		if(removeHeader == true)
+			temp.remove(0);
+		try {
+			fileReader.close();			
+		}catch(Exception e){
+			
 		}
-		inputStream.close();
+		
 		return temp;
 	}
 	
